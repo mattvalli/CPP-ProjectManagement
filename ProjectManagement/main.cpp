35cfa0002c07c941262546b0dccdc0b78d1161fb
Matthew Valli <matthew.valli@gmail.com>
2015-03-09 16:54:40 -0700

    Initial Commit

---
+++ ProjectManagement/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProjectManagement
+//
+//  Created by Matthew Valli on 9/03/2015.
+//  Copyright (c) 2015 Matthew Valli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
